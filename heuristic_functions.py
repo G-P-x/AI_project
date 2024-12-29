303f855f691a9e3004f153b91e5f9fb486ab5efc
GIOVANNI PAU <g.pau15@studenti.unica.it>
2024-12-29 17:55:09 +0100

    UPDATED HEURISTIC FUNCTIONS
    I added new heuristic functions that can be used for this kind of
    problem

--- heuristic_functions.py
+++ heuristic_functions.py
@@ -1,4 +1,5 @@
 def h1(goal: dict, state: list) -> int:
+    '''Not admissible heuristic function'''
     assert(isinstance(goal, dict), "goal must be a dictionary")
     assert(isinstance(state, list), "state must be a list")
 
@@ -13,9 +14,9 @@ def h1(goal: dict, state: list) -> int:
             total_distance += distance
 
 def manhattan_distance(goal: dict, state: list) -> int:
+    '''Manhattan distance heuristic function'''
     assert(isinstance(goal, dict), "goal must be a dictionary")
     assert(isinstance(state, list), "state must be a list")
-
     distance = 0
     for row in range(3):
         for col in range(3):
@@ -35,17 +36,44 @@ def heuristic_manhattan_distance(state: list[list[int]], goal: dict) -> int:
     return distance
 
 def h_2(state: list[list[int]], goal: dict):
-        # calculate the number of moves that each tile is from its goal position
-        # for each tile in the current state, calculate the manhattan distance to its goal position
-        # sum all the distances to get the heuristic value
-        distance = 0
-        total_distance = 0
-        for row in range(3):
-            for col in range(3):
-                tile = state[row][col] # get the integer value of the tile
-                if tile != 0:
-                    distance = abs(row - goal[tile][0]) + abs(col - goal[tile][1])
-                    if distance > 1:
-                        distance *= 1.4 # from 1.4 onwards, the heuristic is not admissible for sure
-                    total_distance += distance
-        return total_distance
+    # calculate the number of moves that each tile is from its goal position
+    # for each tile in the current state, calculate the manhattan distance to its goal position
+    # sum all the distances to get the heuristic value
+    assert isinstance(goal, dict), "goal must be a dictionary"
+    assert isinstance(state, list), "state must be a list"
+    distance = 0
+    total_distance = 0
+    for row in range(3):
+        for col in range(3):
+            tile = state[row][col] # get the integer value of the tile
+            if tile != 0:
+                distance = abs(row - goal[tile][0]) + abs(col - goal[tile][1])
+                if distance > 1:
+                    distance *= 1.4 # from 1.4 onwards, the heuristic is not admissible for sure
+                total_distance += distance
+    return total_distance
+
+def bad_heuristic(state: list[list[int]], goal: dict):
+    distance = 0
+    total_distance = 0
+    for row in range(3):
+        for col in range(3):
+            tile = state[row][col] # get the integer value of the tile
+            if tile != 0:
+                distance = abs(row - goal[tile][0]) + abs(col - goal[tile][1])
+                if distance > 1:
+                    distance *= 0.5 # from 1.4 onwards, the heuristic is not admissible for sure
+                total_distance += distance
+    return total_distance
+
+def h_3(state: list[list[int]], goal: dict):
+    '''Count the number of misplaced tiles'''
+    assert isinstance(goal, dict), "goal must be a dictionary"
+    assert isinstance(state, list), "state must be a list"
+    count = 0
+    for row in range(3):
+        for col in range(3):
+            tile = state[row][col]
+            if tile != 0 and tile != goal[tile]:
+                count += 1
+    return count
